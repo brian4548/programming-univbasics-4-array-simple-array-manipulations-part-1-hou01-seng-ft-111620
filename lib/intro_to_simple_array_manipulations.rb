5592df0eba107365f34701938165f693bf1fba1e
Brian Rhodes <brianr4548@gmail.com>
2020-10-01 04:29:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,8 +14,8 @@ def pop_with_args(dog_breeds)
   dog_breeds.pop(2)
 end
 
-def using_shift
-  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+def using_shift()
+  @ = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
     @im_so_over_this_city = using_shift(@my_favorite_cities)
 
 
