a29712a842668a2fcc51830716551aa0d97b08e5
Brian Rhodes <brianr4548@gmail.com>
2020-10-01 03:27:28 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array,string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan","Bronx"]
+  new_bourough = "Staten Island"
+  bouroughs_in_nyc.unshift(new_bourough)
+end
+
+def using_pop(continents)
+ continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+ continents.pop
+end
+
+
+
+
+
+
+  
+  
+  
+  
+  
