6bfb38d5ca5adb258cb5acdac5054fefdf0a2c57
Brian Rhodes <brianr4548@gmail.com>
2020-10-01 04:21:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,4 @@
-def using_push(array,string)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  next_color = "violet"
+def using_push(colors_in_the_rainbow,next_color)
   colors_in_the_rainbow.push(next_color)
 end
 
@@ -11,6 +9,7 @@ end
 def using_pop(continents)
  continents.pop
 end
+
 
 
 
